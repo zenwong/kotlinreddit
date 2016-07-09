50392f796cda84c115d37d6ff891d849b2b7f4e0
Zen Wong <zenwong@gmail.com>
2016-07-09 11:58:33 +0800

    comments activity select from sqlite working

--- app/src/main/java/com/example/zen/kotlinreddit/CommentsActivity.kt
+++ app/src/main/java/com/example/zen/kotlinreddit/CommentsActivity.kt
@@ -1,19 +1,20 @@
 package com.example.zen.kotlinreddit
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.example.zen.kotlinreddit.fragments.CommentsAdapter
 import com.example.zen.kotlinreddit.models.Comment
-import com.example.zen.kotlinreddit.models.CommentHeader
 import kotlinx.android.synthetic.main.comments.*
 import kotlinx.android.synthetic.main.main.*
+import rx.Observable
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 import rx.subscriptions.CompositeSubscription
 
 class CommentsActivity : AppCompatActivity() {
-	val subscriptions = CompositeSubscription()
+	var subscriptions = CompositeSubscription()
 	val table = "comments"
 	val select = "select * from comments where parent = ?"
 	val layout = LinearLayoutManager(this)
@@ -29,22 +30,24 @@ class CommentsActivity : AppCompatActivity() {
 		list.layoutManager = layout
 		list.adapter = adapter
 
-		subscriptions.add(App.sdb.createQuery(table, select, pid.toString())
+		val url = intent.dataString
+		val permalink = Uri.parse(url).path
+		val link = "${Reddit.REDDIT_FRONT}$permalink.json"
+		val parent = Uri.parse(url).pathSegments[3]
+		println("ZXZ $link")
+
+		subscriptions.add(Observable.fromCallable { Reddit.parseComments(link, parent) }
+			.subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread()).subscribe())
+
+		subscriptions.add(App.sdb.createQuery(table, select, parent)
 			.mapToList(Comment.MAPPER)
 			.subscribeOn(Schedulers.newThread())
 			.observeOn(AndroidSchedulers.mainThread())
 			.subscribe(adapter))
+	}
 
-		subscriptions.add(App.sdb.createQuery("comment_headers", "select * from comment_headers where parent = ? limit 1", pid)
-			.mapToOne(CommentHeader.MAPPER)
-			.subscribeOn(Schedulers.newThread())
-			.observeOn(AndroidSchedulers.mainThread())
-			.subscribe {
-				println("TTT: media: ${it.media} preview: ${it.best}")
-
-//				txtCommentHeaderTitle.text = it.title
-//				txtCommentHeaderAuthor.text = it.author
-//				txtCommentHeaderSelfText.text = it.selftext
-			})
+	override fun onStop() {
+		subscriptions.clear()
+		super.onStop()
 	}
 }
